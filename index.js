8a0c575037c840494c86769cc0a9326c034b26ab
cochiletSerban <cochiletSerban@gmail.com>
2018-10-18 20:04:05 +0300

    added eslint config file

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-const port = process.env.PORT || 3000;
-const io = require("socket.io");
+const io = require('socket.io')
 
-
-const server = io.listen(port);
-
-//socket
+const port = process.env.PORT || 3000
+const server = io.listen(port)
 
 server.on('connection', (socket) => {
+  console.log('connection made on socket:', socket.id)
 
-  console.log("connection made on socket:" ,socket.id);
+  socket.on('chatMsg', (data) =>{
+    server.sockets.emit('chatMsg', data)
+  })
 
-  socket.on('chatMsg',(data) =>{
-    server.sockets.emit('chatMsg',data);
-  });
-
-  socket.on('typing' ,(data) => {
-    socket.broadcast.emit('typing',data);
-  });
-
-});
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', data)
+  })
+})
