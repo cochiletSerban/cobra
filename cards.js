d9fe4f62c9d4312263d064b203a701769fdc42f1
alexkratos <alex.kratos08@gmail.com>
2018-10-22 19:14:44 +0300

    Add files via upload

---
+++ cards.js
@@ -0,0 +1,617 @@
+var cardType = Object.freeze({"player":1, "coach":2, "func":3}) // card type enum
+ 
+const cards = [ // card list 
+    {   // card object structure for player card 
+        type: cardType.player,
+        name: "Lionel Messi",
+        atack: 9,
+        defence: 2,
+        nationality: "Argentina",
+        team: "Barcelona",
+		role: 3,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/158023.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Ronaldo",
+        atack: 9,
+        defence: 3,
+        nationality: "Portugal",
+        team: "Juventus",
+		role: 3,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/20801.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Neymar",
+        atack: 9,
+        defence: 2,
+        nationality: "Brazil",
+        team: "Paris Saint-Germain",
+		role: 3,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/190871.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Suárez",
+        atack: 9,
+        defence: 2,
+        nationality: "Argentina",
+        team: "Barcelona",
+		role: 3,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/176580.png"
+    },
+	{   
+        type: cardType.player,
+        name: "De Bruyne",
+        atack: 8,
+        defence: 6,
+        nationality: "Belgium",
+        team: "Manchester City",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/192985.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Ramos",
+        atack: 5,
+        defence: 9,
+        nationality: "Spain",
+        team: "Real Madrid",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/155862.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Modric",
+        atack: 8,
+        defence: 6,
+        nationality: "Croatia",
+        team: "Real Madrid",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/177003.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Hazard",
+        atack: 9,
+        defence: 3,
+        nationality: "Belgium",
+        team: "Chelsea",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/p50514925.png"
+    },
+	{   
+        type: cardType.player,
+        name: "De Gea",
+        atack: 1,
+        defence: 9,
+        nationality: "Spain",
+        team: "Manchester United",
+		role: 0,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/193080.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Kroos",
+        atack: 7,
+        defence: 7,
+        nationality: "Germany",
+        team: "Real Madrid",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/182521.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Dybala",
+        atack: 9,
+        defence: 3,
+        nationality: "Argentina",
+        team: "Juventus",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/211110.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Silva",
+        atack: 7,
+        defence: 5,
+        nationality: "Spain",
+        team: "Manchester City",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/168542.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Isco",
+        atack: 8,
+        defence: 6,
+        nationality: "Spain",
+        team: "Real Madrid",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/197781.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Pogba",
+        atack: 8,
+        defence: 7,
+        nationality: "France",
+        team: "Manchester United",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/195864.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Kanté",
+        atack: 7,
+        defence: 8,
+        nationality: "France",
+        team: "Chelsea",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/215914.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Rodríguez",
+        atack: 8,
+        defence: 5,
+        nationality: "Colombia",
+        team: "Bayern München",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/198710.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Casemiro",
+        atack: 6,
+        defence: 8,
+        nationality: "Brazil",
+        team: "Real Madrid",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/200145.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Hamšik",
+        atack: 7,
+        defence: 7,
+        nationality: "Slovakia",
+        team: "Napoli",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/171877.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Rakitic",
+        atack: 7,
+        defence: 7,
+        nationality: "Croatia",
+        team: "Barcelona",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/168651.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Rakitic",
+        atack: 7,
+        defence: 7,
+        nationality: "Croatia",
+        team: "Barcelona",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/168651.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Mbappe",
+        atack: 9,
+        defence: 4,
+        nationality: "France",
+        team: "Paris Saint-Germain",
+		role: 3,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/p67340611.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Reus",
+        atack: 8,
+        defence: 5,
+        nationality: "Germany",
+        team: "Borussia Dortmund",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/p84074430.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Pjanic",
+        atack: 7,
+        defence: 7,
+        nationality: "Bosnia and Herzegovina",
+        team: "Juventus",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/180206.png"
+    },
+	{   
+        type: cardType.player,
+        name: "Matic",
+        atack: 7,
+        defence: 8,
+        nationality: "Serbia",
+        team: "Manchester United",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/191202.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Neuer",
+        atack: 2,
+        defence: 9,
+        nationality: "Germany",
+        team: "Bayern München",
+		role: 0,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/167495.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Courtois",
+        atack: 1,
+        defence: 9,
+        nationality: "Belgium",
+        team: "Real Madrid",
+		role: 0,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/192119.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Navas",
+        atack: 1,
+        defence: 8,
+        nationality: "Costa Rica",
+        team: "Real Madrid",
+		role: 0,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/193041.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Cech",
+        atack: 1,
+        defence: 9,
+        nationality: "Czech Republic",
+        team: "Arsenal",
+		role: 0,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/p50380588.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Lewandowski",
+        atack: 9,
+        defence: 4,
+        nationality: "Poland",
+        team: "Bayern München",
+		role: 3,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/188545.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Agüero",
+        atack: 9,
+        defence: 2,
+        nationality: "Argentina",
+        team: "Manchester City",
+		role: 3,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/153079.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Kane",
+        atack: 9,
+        defence: 4,
+        nationality: "England",
+        team: "Tottenham Hotspur",
+		role: 3,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/202126.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Cavani",
+        atack: 8,
+        defence: 5,
+        nationality: "Uruguay",
+        team: "Paris Saint-Germain",
+		role: 3,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/179813.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Griezmann",
+        atack: 8,
+        defence: 5,
+        nationality: "France",
+        team: "Atlético Madrid",
+		role: 3,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/194765.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Verratti",
+        atack: 6,
+        defence: 8,
+        nationality: "Italy",
+        team: "Paris Saint-Germain",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/199556.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Perišic",
+        atack: 8,
+        defence: 6,
+        nationality: "Croatia",
+        team: "Inter",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/181458.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Vidal",
+        atack: 8,
+        defence: 8,
+        nationality: "Chile",
+        team: "Barcelona",
+		role: 2,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/181872.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Godín",
+        atack: 4,
+        defence: 9,
+        nationality: "Uruguay",
+        team: "Atlético Madrid",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/182493.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Chiellini",
+        atack: 4,
+        defence: 9,
+        nationality: "Italy",
+        team: "Juventus",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/138956.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Hummels",
+        atack: 5,
+        defence: 9,
+        nationality: "Germany",
+        team: "Bayern München",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/178603.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Marcelo",
+        atack: 7,
+        defence: 8,
+        nationality: "Brazil",
+        team: "Real Madrid",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/176676.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Silva",
+        atack: 5,
+        defence: 8,
+        nationality: "Brazil",
+        team: "Paris Saint-Germain",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/164240.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Piqué ",
+        atack: 5,
+        defence: 8,
+        nationality: "Spain",
+        team: "Barcelona",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/152729.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Umtiti",
+        atack: 5,
+        defence: 8,
+        nationality: "France",
+        team: "Barcelona",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/205600.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Vertonghen",
+        atack: 6,
+        defence: 8,
+        nationality: "Belgium",
+        team: "Tottenham Hotspur",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/172871.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Koulibaly",
+        atack: 2,
+        defence: 8,
+        nationality: "Senegal",
+        team: "Napoli",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/201024.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Benatia",
+        atack: 4,
+        defence: 8,
+        nationality: "Morocco",
+        team: "Juventus",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/177509.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Otamendi",
+        atack: 5,
+        defence: 8,
+        nationality: "Argentina",
+        team: "Manchester City",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/192366.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Boateng",
+        atack: 5,
+        defence: 8,
+        nationality: "Germany",
+        team: "Bayern München",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/183907.png	"
+    },
+    {   
+        type: cardType.player,
+        name: "Varane",
+        atack: 4,
+        defence: 8,
+        nationality: "France",
+        team: "Real Madrid",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/201535.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Kompany",
+        atack: 5,
+        defence: 8,
+        nationality: "Belgium",
+        team: "Manchester City",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/139720.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Alaba",
+        atack: 7,
+        defence: 8,
+        nationality: "Austria",
+        team: "Bayern München",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/197445.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Kimmich",
+        atack: 6,
+        defence: 7,
+        nationality: "Germany",
+        team: "Bayern München",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/212622.png"
+    },
+    {   
+        type: cardType.player,
+        name: "van Dijk",
+        atack: 4,
+        defence: 8,
+        nationality: "Holland",
+        team: "Liverpool",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/203376.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Manolas",
+        atack: 1,
+        defence: 9,
+        nationality: "Greece",
+        team: "Roma",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/192774.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Barzagli",
+        atack: 3,
+        defence: 9,
+        nationality: "Italy",
+        team: "Juventus",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/137186.png"
+    },
+    {   
+        type: cardType.player,
+        name: "Süle",
+        atack: 4,
+        defence: 8,
+        nationality: "Germany",
+        team: "Bayern München",
+		role: 1,
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/212190.png"
+    },
+    {   // card object structure for player card 
+        type: cardType.coach,
+        name: "Maradona",
+        passive: "", // leave blank
+        active: "", // leave blank
+        nationality: "Argentina",
+        team: "",
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/190042.png"
+    },
+    {   // card object structure for player card 
+        type: cardType.coach,
+        name: "van Basten",
+        passive: "", // leave blank
+        active: "", // leave blank
+        nationality: "Holland",
+        team: "",
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/192181.png"
+    },
+    {   // card object structure for player card 
+        type: cardType.coach,
+        name: "Blanc",
+        passive: "", // leave blank
+        active: "", // leave blank
+        nationality: "France",
+        team: "",
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/238443.png"
+    },
+    {   // card object structure for player card 
+        type: cardType.coach,
+        name: "Shevchenko",
+        passive: "", // leave blank
+        active: "", // leave blank
+        nationality: "Ucraina",
+        team: "",
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/13128.png"
+    },
+    {   // card object structure for player card 
+        type: cardType.coach,
+        name: "Nedved",
+        passive: "", // leave blank
+        active: "", // leave blank
+        nationality: "Czech Republic",
+        team: "",
+        picture: "https://cdn.futbin.com/content/fifa19/img/players/6235.png"
+    },
+    {   // card object structure for player card 
+        type: cardType.func,
+        name: "",
+        active: "", // leave blank
+        nationality: "",
+        team: "",
+        picture: ""
+    }
+]
