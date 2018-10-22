8de199a2f4831ddc24c6a382eb988395ecbd6635
cochiletSerban <cochiletSerban@gmail.com>
2018-10-22 20:45:25 +0300

    added lobby

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const io = require('socket.io')
 const port = process.env.PORT || 3000
 const server = io.listen(port)
 
+function isLobbyFull () {
+  return server.engine.clientsCount == 2
+}
+
+function moveToSelectCards (socket) {
+  server.emit('lobbyFull')
+}
+
 server.on('connection', (socket) => {
   console.log('connection made on socket:', socket.id)
-
-  socket.on('chatMsg', (data) => {
-    server.sockets.emit('chatMsg', data)
-  })
-
-  socket.on('typing', (data) => {
-    socket.broadcast.emit('typing', data)
-  })
+  if (isLobbyFull()) {
+    moveToSelectCards(socket)
+  }
 })
