4ac266adb6d1c1dfbcbf99423c3ed472c59cdc05
cochiletSerban <cochiletSerban@gmail.com>
2018-10-23 19:11:21 +0300

    added handles for connect and disconect events

--- index.js
+++ index.js
@@ -4,17 +4,36 @@ const cardList = require('./assets/cards')
 const port = process.env.PORT || 3000
 const server = io.listen(port)
 
-function isLobbyFull () {
+function isLobbyToFull () {
+  return server.engine.clientsCount > 2
+}
+
+function isLobbyReady () {
   return server.engine.clientsCount == 2
 }
 
+function kickExtraConnections (socket) {
+  if (isLobbyToFull()) {
+    socket.disconnect(true)
+    console.log('kicked socket:', socket.id)
+  }
+}
+
 function moveToSelectCards (socket) {
-  server.emit('lobbyFull')
+  server.emit('lobbyFull', cardList.card)
 }
 
 server.on('connection', (socket) => {
   console.log('connection made on socket:', socket.id)
-  if (isLobbyFull()) {
+  console.log(server.engine.clientsCount)
+  kickExtraConnections(socket)
+  if (isLobbyReady()) {
     moveToSelectCards(socket)
   }
+
+  socket.on('disconnect', (socket) => {
+    console.log('connection severed on socket:', socket.id)
+    console.log(server.engine.clientsCount)
+  })
+
 })
